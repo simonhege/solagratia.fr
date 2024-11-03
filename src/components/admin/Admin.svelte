5a94ba8a337ce6ef09b07f660a22a00e066f41be
Simon HEGE <simonhege@gmail.com>
2024-11-03 07:11:01 +0100

    feat(admin): display page view stats

---
+++ src/components/admin/Admin.svelte
@@ -0,0 +1,56 @@
+<div class="w-full flex-grow flex flex-col gap-4">
+  <div class="bg-white border-gray-900 shadow-xl p-4 flex items-center justify-between">
+    <div class="flex items-center gap-2">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+      </svg>        
+      <label for="period" >Période: </label>
+      <select bind:value={inputPeriod}>
+        <option value="day">Jour</option>
+        <option value="month">Mois</option>
+      </select>
+    </div>
+    <h2 class="font-bold text-[#111827] text-2xl xl:text-2xl">Statistics</h2>
+    <div class="flex items-center gap-2">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
+      </svg>        
+      <label for="api-key" >Clé: </label>
+      <input type="password" name="api-key" bind:value={inputApiKey} class="focus:outline-none focus:ring focus:ring-indigo-200 focus:border-indigo-500" />
+    </div>
+  </div>
+  <Table stats={pageViewedStats}></Table>
+</div>
+
+<script lang="ts">
+  import Table from "./Table.svelte";
+
+  type Stats = {
+    headers?: String[],
+    values?: Map<String, number[]>
+  }
+
+  let inputPeriod = $state("day")
+  let inputApiKey = $state("")
+  let pageViewedStats : Stats = $state({})
+
+  $effect(() => {
+    if (inputApiKey.length > 0) {
+      getPageViewedStats(inputPeriod, inputApiKey);
+    }
+  });
+
+  async function getPageViewedStats(period: string, apiKey: string) {
+    const raw = await window.fetch("https://api.solagratia.fr/admin/pageViewed/stats?period="+period, {
+      headers: {
+        'X-Api-Key': apiKey,
+      }
+    });
+    if (raw.ok) {
+      const jsonData: Stats = await raw.json();
+      pageViewedStats = jsonData;
+    } else {
+      console.error(`HTTP error! Status: ${raw.status}`)
+    }
+  }
+</script>
