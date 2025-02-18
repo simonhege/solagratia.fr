39cb07354955ffa6fc3f58e2677d37513e1b9ea2
Simon HEGE <simonhege@gmail.com>
2025-02-18 08:18:36 +0100

    fix(admin): textes en francais

--- src/components/admin/Admin.svelte
+++ src/components/admin/Admin.svelte
@@ -10,7 +10,7 @@
         <option value="month">Mois</option>
       </select>
     </div>
-    <h2 class="font-bold text-[#111827] text-2xl xl:text-2xl">Statistics</h2>
+    <h2 class="font-bold text-[#111827] text-2xl xl:text-2xl">Statistiques</h2>
     <div class="flex items-center gap-2">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
@@ -21,7 +21,7 @@
   </div>
   <Table stats={pageViewedStats}></Table>
   <div class="bg-white border-gray-900 shadow-xl p-4 flex items-center justify-between">
-    <h2 class="font-bold text-[#111827] text-2xl xl:text-2xl">History</h2>
+    <h2 class="font-bold text-[#111827] text-2xl xl:text-2xl">Historique</h2>
   </div>
   <div class="bg-white border-gray-900 shadow-xl p-4">
     <table class="table-auto w-full p-1">
