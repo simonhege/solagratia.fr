108510c7ece08cef0613482f926c866c6587b0ca
Simon HEGE <simonhege@gmail.com>
2025-06-09 06:08:01 +0200

    fix: use fetch as expected by sveltekit

--- src/routes/(app)/bible/+page.ts
+++ src/routes/(app)/bible/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export const prerender = false;
 
-export const load: PageLoad = async () => {
+export const load: PageLoad = async ({fetch}) => {
 	const res = await fetch('https://api.solagratia.fr/bible');
 	return await res.json();
 };
--- src/routes/(app)/bible/[bookCode]/[chapter]/+page.ts
+++ src/routes/(app)/bible/[bookCode]/[chapter]/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export const prerender = false;
 
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({ fetch, params }) => {
 	const res = await fetch(
 		'https://api.solagratia.fr/bible/' + params.bookCode + '/' + params.chapter
 	);
